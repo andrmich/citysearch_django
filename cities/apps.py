c3e413a29f65cc2b380a881777da4d00463df855
Michalina Andrzejewska <andrmichgithub@mismail.xyz>
2019-05-15 19:03:10 +0200

    Created model, maigrated to db, created superuser for easy db population.

--- cities/apps.py
+++ cities/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
+from django.contrib import admin
+from .models import City
 
 
 class CitiesConfig(AppConfig):
     name = 'cities'
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "state",)
+
+
+admin.site.register(City, CityAdmin)
