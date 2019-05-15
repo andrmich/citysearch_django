e19e8fbb0c2df9873b58a52e3b18f9590235183d
Michalina Andrzejewska <andrmichgithub@mismail.xyz>
2019-05-15 19:55:42 +0200

    Added a form to homepage and basic query idea to search_results.html.

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views.generic import TemplateView, ListView
 
 from cities.models import City
@@ -10,3 +11,9 @@ class HomePageView(TemplateView):
 class SearchResultsView(ListView):
     model = City
     template_name = "search_results.html"
+
+    def get_queryset(self):
+        return City.objects.filter(
+            Q(name__icontains='Boston') | Q(state__icontains='NY')
+        )
+
